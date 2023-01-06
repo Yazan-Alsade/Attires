d6bd5c62fae991e7463dd78a107cf90ef3cc12a3
Yazan-Alsade <yazanalsade327@gmail.com>
2023-01-06 20:02:33 +0200

    edit Attires

--- js/script.js
+++ js/script.js
@@ -16,3 +16,15 @@ function resetFilterBtns(){
     $(this).removeClass('active-filter-btn');
   });
 }
+const observer = new IntersectionObserver((entries)=>{
+entries.forEach((entry)=>{
+  console.log(entry);
+  if(entry.isIntersecting){
+    entry.target.classList.add('show');
+  } else{
+    entry.target.classList.remove('show');
+  }
+});
+});
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el)=>observer.observe(el));
